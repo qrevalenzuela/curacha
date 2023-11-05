fa7a9b24cf8d255731a7a79b878324e689dbb56d
qrevalenzuela <qrevalenzuela@tip.edu.ph>
2023-11-05 13:51:54 +0800

    Request a Quote in NavBar should route to ReqQuote

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import ReqQuote from "./components/ReqQuote/ReqQuote";
@@ -8,14 +8,23 @@ import Landing from "./components/Landing/Landing";
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Landing />} />
-          <Route path="/req-quote" element={<ReqQuote />} />
+          <Route
+            path="/"
+            element={
+              <div>
+                <Navbar />
+                <Outlet />
+                <Footer />
+              </div>
+            }
+          >
+            <Route index element={<Landing />} />
+            <Route path="/req-quote" element={<ReqQuote />} />
+          </Route>
         </Routes>
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import './NavBar.css';
-import logo from '../../assets/logo-curacha.png';
+import React from "react";
+import "./NavBar.css";
+import logo from "../../assets/logo-curacha.png";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="navbar">
       <img src={logo} alt="Logo" className="logo" />
       <div className="menu">
-        <a href="#home" className="menuItem">Customize Order</a>
-        <a href="#about" className="menuItem">Items</a>
-        <a href="#services" className="menuItem">Services</a>
-        <a href="#contact" className="menuItem">Login</a>
+        <Link to="/req-quote" className="menuItem">
+          Request a Quote
+        </Link>
+
+        <a href="#about" className="menuItem">
+          Items
+        </a>
+        <a href="#services" className="menuItem">
+          Services
+        </a>
+        <a href="#contact" className="menuItem">
+          Login
+        </a>
       </div>
     </div>
   );
-}
+};
 
 export default NavBar;
